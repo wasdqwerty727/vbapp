b234cd442ae66ecef0f12e12533a0f2b02484f5f
Beksultan <bekijanmamatov@gmail.com>
2024-11-29 02:00:04 +0600

    Initial commit

---
+++ src/main/java/com/example/vbapp/VbappApplication.java
@@ -0,0 +1,13 @@
+package com.example.vbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbappApplication.class, args);
+	}
+
+}
